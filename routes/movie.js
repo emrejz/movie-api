7d7acca325e09289b3afa0c9a4e0876083542b1b
emrejz <emre.kayacan@msn.com>
2019-01-04 17:40:53 +0300

    delete/update/find_byId routers and top10/betweenYears routers added

--- routes/movie.js
+++ routes/movie.js
@@ -13,7 +13,71 @@ router.get('/',(req,res,next)=>{
     res.json(err)
   })
 })
+router.get('/between/:start_year/:end_year',(req,res,next)=>{
+  const {start_year,end_year}=req.params;
+  const promise=Movie.find(
+    {
+      year:{"$gte":parseInt(start_year),"$lte":parseInt(end_year)}}
+    );
+  promise.then(data=>{
+    res.json(data)
+  })
+  .catch(err=>{
+    res.json(err)
+  })
+})
+router.get('/top10',(req,res,next)=>{
+  const promise=Movie.find({}).sort({imdb_score:-1}).limit(10);
+  promise.then(data=>{
+    if(!data)
+    next({message:"Top list was not found "})
+    res.json(data)
+  })
+    .catch(err=>{
+      res.json(err)
+    })
+})
 
+router.get('/:movie_id',(req,res,next)=>{
+   
+  const promise=Movie.findById(req.params.movie_id);
+  promise.then((data)=>{
+    if(!data)
+    next({message:'The movie was not found!',code:1});
+    res.json(data)
+  })
+  .catch(err=>{
+    res.json(err)
+  })
+})
+router.put('/:movie_id',(req,res,next)=>{
+  const promise=Movie.findByIdAndUpdate(
+    req.params.movie_id ,
+    req.body,
+    {
+      new:true //güncel datayı döndürür 
+    });
+  promise.then(data=>{
+    if(!data)
+    next({message:"The movie was not found!",code:1});
+    res.json(data);
+  })
+  .catch(err=>{
+    res.json(err)
+  })
+})
+
+router.delete('/:movie_id',(req,res,next)=>{
+  const promise=Movie.findByIdAndRemove(req.params.movie_id);
+  promise.then(data=>{
+    if(!data)
+    next({message:"The movie was not found!"})
+    res.json({status:1});
+  })
+  .catch(err=>{
+    res.json(err)
+  })
+})
 
 
 
