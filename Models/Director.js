f453adde70c6fb2535cca909b097197bf3066ea9
emrejz <emre.kayacan@msn.com>
2019-01-05 22:49:21 +0300

    director and  movie's validations added

--- Models/Director.js
+++ Models/Director.js
@@ -4,11 +4,17 @@ const Schema=mongoose.Schema;
 const DirectorSchema=new Schema({
     name:{
         type:String,
-        required:true
+        required:[true, '`{PATH}` alanı boş olamaz'],
+        maxlength:[30, '`{PATH}` alanı max 30 karakter olabilir'],
+        minlength:[2, '`{PATH}` alanı min 2 karakter olabilir'],
+        trim: true
     },
     surname:{
         type:String,
-        required:true
+        required:[true, '`{PATH}` alanı boş olamaz'],
+        maxlength:[30, '`{PATH}` alanı max 30 karakter olabilir'],
+        minlength:[2, '`{PATH}` alanı min 2 karakter olabilir'],
+        trim: true
     },
     createdAt:{
         type:Date,
--- Models/Movie.js
+++ Models/Movie.js
@@ -5,20 +5,40 @@ const MovieSchema=new Schema({
     director_id:Schema.Types.ObjectId,
     title:{
         type:String,
-        required:true,
-        unique:true
+        required:[true, '`{PATH}` alanı boş olamaz'],
+        unique:[true, '`{PATH}` alanı benzersiz olmalı'],
+        maxlength:[30, '`{PATH}` alanı max 30 karakter olabilir'],
+        minlength:[1, '`{PATH}` alanı min 1 karakter olabilir'],
+        trim: true
     },
     category:{
-        type:String
+        type:String,
+        required:[true, '`{PATH}` alanı boş olamaz'],
+        maxlength:[30, '`{PATH}` alanı max 30 karakter olabilir'],
+        minlength:[1, '`{PATH}` alanı min 1 karakter olabilir'],
+        trim: true
     },
     country:{
-        type:String
+        type:String,
+        required:[true, '`{PATH}` alanı boş olamaz'],
+        maxlength:[30, '`{PATH}` alanı max 30 karakter olabilir'],
+        minlength:[1, '`{PATH}` alanı min 1 karakter olabilir'],
+        trim: true
     },
     year:{
-        type:Number
+        type:Number,
+        required:[true, '`{PATH}` alanı boş olamaz'],
+        max:2020,
+        min:1800,
+        trim: true
     },
     imdb_score:{
-        type:Number
+        type:Number,  
+        required:[true, '`{PATH}` alanı boş olamaz'],
+        max:10,
+        min:0,
+        trim: true
+        
     },
 
     createdAt:{
