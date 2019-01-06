0817f6d38529878d37ce589b6776fe6ed187d3e6
emrejz <emre.kayacan@msn.com>
2019-01-07 00:10:19 +0300

    /PUT/:movie_id test added

--- test/movie-test.js
+++ test/movie-test.js
@@ -62,6 +62,7 @@
      })
      describe("/GET/:movie_id",()=>{
          it("GET movie by movie_id",(done)=>{
+            
             chai.request(server)
             .get('/api/movies/'+movie_id)
             .set('x-access-token',token)
@@ -74,10 +75,35 @@
                 res.body.should.have.property('year');
                 res.body.should.have.property('imdb_score');
                 res.body.should.have.property('country');
-                res.body.should.have.property('_id').eq(movie_id);
+                res.body.should.have.property('_id').eql(movie_id);
                 done();
 
             })
+         })
+     })
+     describe("/PUT/:movie_id movie",()=>{
+         it("Update movie by movie id",(done)=>{
+            const movie={
+                title:"TEST1",
+                director_id:"5c30d4a30697d313e00b3411",
+                category:"TEST1",
+                country:"Turkey1",
+                year:2001,
+                imdb_score:2
+            }
+             chai.request(server)
+             .put('/api/movies/'+movie_id)
+             .send(movie)
+             .set("x-access-token",token)
+             .end((err,res)=>{
+                 res.body.should.have.property('title').eql(movie.title);
+                 res.body.should.have.property('director_id').eql(movie.director_id);
+                 res.body.should.have.property('category').eql(movie.category);
+                 res.body.should.have.property('year').eql(movie.year);
+                 res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+                 res.body.should.have.property('country').eql(movie.country);
+                 done()
+             })
          })
      })
  	})
