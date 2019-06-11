6af64154f176bcdb928a5eba228d9a952b3d3552
emrejz <emre.kayacan@msn.com>
2019-06-12 00:24:02 +0300

    cors added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
 
 const authenticateRouter = require('./routes/authenticate');
@@ -15,6 +16,7 @@ const indexRouter=require("./routes/index")
 const verifyToken=require('./middleware/verify-token');
 
 const app = express();
+app.use(cors())
 
 //db connection
 const db=require('./helper/db');
