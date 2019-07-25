b702e384d7f78e814397aed450e589d89bbf931d
emre <43180571+emrejz@users.noreply.github.com>
2019-07-25 15:46:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const indexRouter=require("./routes/index")
 //middlevare verifyToken
 const verifyToken=require('./middleware/verify-token');
 const app = express();
-app.use(cors())
+app.use(cors({origin:true}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
