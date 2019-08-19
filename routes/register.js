abdf6958ce3b12128475eb1528d4c7717f87248f
emre <43180571+emrejz@users.noreply.github.com>
2019-08-19 18:34:15 +0300

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -42,7 +42,7 @@ router.post("/", (req, res, next) => {
                     secure: false,
                     maxAge: 24 * 60 * 60 * 1000
                   });
-                  res.json(token);
+                  res.json({token});
                 }
               }
             );
